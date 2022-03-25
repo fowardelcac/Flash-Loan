f9585e238cf5d36ff8195f87656dc1eaa585de41
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2022-03-25 11:18:00 -0300

    Add files via upload

---
+++ Flash.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import "contracts/FlashLoanSimpleReceiverBase.sol";
+import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Flash is FlashLoanSimpleReceiverBase, Ownable {
+    constructor(IPoolAddressesProvider _provider)
+        FlashLoanSimpleReceiverBase(_provider)
+    {}
+
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        uint256 _premium = SafeMath.add(amount, premium);
+
+        IERC20(asset).approve(address(POOL), _premium);
+        return true;
+    }
+
+    function flashBorrow(address _asset, uint256 _amount) external onlyOwner {
+        address _receiverAddress = address(this);
+        bytes memory _params = "";
+        uint16 _referralCode = 0;
+
+        POOL.flashLoanSimple(
+            _receiverAddress,
+            _asset,
+            _amount,
+            _params,
+            _referralCode
+        );
+    }
+
+    function withdraw(address _asset) external onlyOwner {
+        uint256 _balance = IERC20(_asset).balanceOf(address(this));
+        require(_balance > 0, "Wrong balance");
+        IERC20(_asset).transfer(msg.sender, _balance);
+    }
+}
